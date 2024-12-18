7e3965c5ece21f6ace5f4d9ac543062fd2b187cd
Сизиков Семён <Tyuiu.SizikovSS@outlook.com>
2024-12-18 20:22:03 +0500

    Task1

--- Tyuiu.SizikovSS.Sprint5.Task1.V30.Lib/DataService.cs
+++ Tyuiu.SizikovSS.Sprint5.Task1.V30.Lib/DataService.cs
@@ -7,24 +7,24 @@ namespace Tyuiu.SizikovSS.Sprint5.Task1.V30.Lib
     {
         public string SaveToFileTextData(int startValue, int stopValue)
         {
-            int count = 0;
-            for (int x = startValue; x <= stopValue; x++) count++;
-            double[] mass = new double[count];// массив на 11 ячеек
-            int k = 0;
-            string path = Path.Combine(Path.GetTempPath(), @"C:\Users\simen\source\repos\Tyuiu.SizikovSS.Sprint5\OutPutFileTask1.txt");
-            double y;
+            string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask1.txt");
+            double temp;
+            string strtemp;
+
+            FileInfo fileInfo = new(path);
+            bool fileExists = fileInfo.Exists;
+
+            if (fileExists) File.Delete(path);
+
             for (int x = startValue; x <= stopValue; x++)
             {
-                if (2 - 2*x == 0)
-                {
-                    File.WriteAllText(path, Convert.ToString(0) + "\n");
-                    mass[k] = 0;
-                    continue;
-                }
-                y = Math.Round(( Math.Cos(x)+(Math.Sin(x)/2-2*x) + 2*x + 2 ), 2);
-                File.AppendAllText(path, Convert.ToString(y) + "\n");
-                mass[k] = Math.Round(((5 * x + 2.5) / (Math.Sin(x) + 2) + 2 * x + 2), 2);
-                k++;
+                temp = Math.Round((Math.Cos(x) + (Math.Sin(x) / (2 - 2 * x)) - 4 * x), 2);
+                if (x == 1) temp = 0;
+
+                strtemp = Convert.ToString(temp);
+
+                if (x != stopValue) File.AppendAllText(path, strtemp + Environment.NewLine);
+                else File.AppendAllText(path, strtemp);
             }
             return path;
         }
